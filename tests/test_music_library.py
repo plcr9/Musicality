57d90c25ac0dddbf299a352c0218fd7be3556d4f
plcr9 <paulcooperpc9@gmail.com>
2023-08-08 16:54:48 +0100

    Mock test added

--- tests/test_music_library.py
+++ tests/test_music_library.py
@@ -1,18 +1,13 @@
+from unittest.mock import Mock
 from lib.music_library import MusicLibrary
 
 '''When multiple tracks added and keyword searched for, tracks that match that keyword are returned'''
 def test_searches_by_keyword():
     library = MusicLibrary()
-    fake_matching = FakeMatchingTrack()
+    fake_matching = Mock()
+    fake_matching.matches.return_value = True
     library.add(fake_matching)
-    fake_not_matching = FakeNotMatchingTrack()
+    fake_not_matching = Mock()
+    fake_not_matching.matches.return_value = False
     library.add(fake_not_matching)
     assert library.search("Dub") == [fake_matching]
-
-class FakeMatchingTrack():
-    def matches(self, keyword):
-        return True
-    
-class FakeNotMatchingTrack():
-    def matches(self, keyword):
-        return False
